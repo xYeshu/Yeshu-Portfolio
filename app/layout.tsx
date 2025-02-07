5f35bf25adb048a7dc09c16d468784383e7739bb
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-02-07 13:56:41 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,4 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import StarsCanvas from "@/components/main/StarBackground";
-import Navbar from "@/components/main/Navbar";
-import Footer from "@/components/main/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Yeshu Wanjari",
-  description: "My Portfolio",
-};
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -18,9 +6,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    
     <html lang="en">
-      <link rel="icon" type="image/ico" sizes="16x16" href="/favicon.ico"></link>
+      <Head>
+        <title>Yeshu Wanjari</title>
+        <meta name="description" content="My Portfolio" />
+        <link rel="icon" type="image/ico" sizes="16x16" href="/favicon.ico" />
+      </Head>
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
