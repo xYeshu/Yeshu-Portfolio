cce69d8c3b92f127affb9bdd08b0e6e013a3f0a5
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-02-07 13:07:44 +0530

    Update Skills.tsx

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -15,7 +15,7 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+      className="flex hidden md:block flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
       style={{ transform: "scale(0.9" }}
     >
       <SkillText />
