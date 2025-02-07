d0ee533a15bc80deddd05979704b2b274bd1b968
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-02-07 15:30:31 +0530

    Update StarBackground.tsx

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -29,7 +29,7 @@ const StarBackground = (props: any) => {
         >
             <PointMaterial
                 transparent
-                color="$fff"
+                color=""
                 size={0.002}
                 sizeAttenuation={true}
                 dethWrite={false}
