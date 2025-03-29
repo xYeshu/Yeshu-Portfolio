3bf327fda6cbf171d70e79825bc1110713e87af1
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-03-29 23:43:40 +0530

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import HeroContent from "../sub/HeroContent";
 
 const Hero = () => {
+  const [isLaptop, setIsLaptop] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(window.innerWidth >= 768); // Adjust breakpoint if needed
+    };
+
+    handleResize(); // Run on mount
+    window.addEventListener("resize", handleResize);
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="relative  flex flex-col h-full w-full" id="about-me">
-      <video
-        autoPlay
-        muted
-        loop
-        className="rotate-180 hidden md:block absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
-      >
-        <source src="/blackhole.webm" type="video/webm" />
-      </video>
+    <div className="relative flex flex-col h-full w-full" id="about-me">
+      {isLaptop && (
+        <video
+          autoPlay
+          muted
+          loop
+          className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
+        >
+          <source src="/blackhole.webm" type="video/webm" />
+        </video>
+      )}
+
       <HeroContent />
     </div>
   );
