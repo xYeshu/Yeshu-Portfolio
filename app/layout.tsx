70f650c9d586ae7f77130795289e1299d1551520
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-03-29 23:54:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+"use client"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
@@ -7,26 +9,34 @@ import Footer from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Yeshu Wanjari",
-  description: "My Portfolio",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isLaptop, setIsLaptop] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(window.innerWidth >= 768); // Show only on screens ≥ 768px
+    };
+
+    handleResize(); // Check screen size on mount
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/ico" sizes="16x16" href="/favicon.ico" />
-      </head>
+      <link rel="icon" type="image/ico" sizes="16x16" href="/favicon.ico"></link>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarsCanvas /> 
+        {isLaptop && <StarsCanvas />} {/* Render only if isLaptop is true */}
         <Navbar />
         {children}
         <Footer />
